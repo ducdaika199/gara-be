902241d8ccf17c8f25373b36d7026577b211c90d
duc.phamminh1 <duc.phamminh1@infinitechnology.com>
2024-03-04 00:32:47 +0700

    register for user

--- services/gara-service/entry-points/api/auth-routes.ts
+++ services/gara-service/entry-points/api/auth-routes.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { createUser, getUserByUserName } from '../../domain/user-use-case';
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
+import 'dotenv/config'
 
 export const authRoutes = () => {
   const router = express.Router();
@@ -40,11 +41,21 @@ export const authRoutes = () => {
 
         const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
         if (passwordMatch) {
-          // let userName = user.userName;
-          // const aToken = generateAccessToken(payload);
-          // const rToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET);
+          const payload = { userName: req.body.userName };
+
+          const aToken = generateAccessToken(payload);
+          const rToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET as string);
+          res.json({ AccessToken: aToken, RefreshToken: rToken, message: 'You are logged-in' });
+        } else {
+
+          res.json({ message: 'Invalid email or password' });
         }
 
+      } else {
+        let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;		//fake password is used just to slow down the time required to send a response to the user
+        await bcrypt.compare(req.body.password, fakePass);
+
+        res.json({ message: 'Invalid email or password' });
       }
     } catch (error) {
       next(error);
@@ -52,9 +63,9 @@ export const authRoutes = () => {
     }
   }
 
-  // function generateAccessToken(payload) {
-  //   return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2m' });
-  // }
+  function generateAccessToken(payload) {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '2m' });
+  }
 
   return router;
 };
